8fa665e0acf18ba60ceed2add4ad25898bfd81f1
Gustavo-C-almeida <105670387+Gustavo-C-almeida@users.noreply.github.com>
2023-04-23 21:25:12 -0300

    Commit entrega 23/04
    Adicionando segundo microserviço de cifras e esqueleto do react.

--- cifra/index.js
+++ cifra/index.js
@@ -5,12 +5,20 @@ const app = express();
 app.use(bodyParser.json());
 
 const cifrasPorMusicaId = {};
+const {v4: uuidv4} = require('uuid');
 
 app.put('/musica/:id/cifra', (req, res) => {
-
+    const idCifra = uuidv4();
+    const { texto } = req.body
+    const cifrasDaMusica =
+    cifrasPorMusicaId[req.params.id] || [];
+    cifrasDaMusica.push({ id: idCifra, texto });
+    cifrasPorMusicaId[req.params.id] =
+    cifrasDaMusica;
+    res.status(201).send(cifrasDaMusica);
 });
 
-app.get('/lembretes/:id/cifra', (req, res) => {
+app.get('/musica/:id/cifra', (req, res) => {
 
 });
 
