7f8aa2c8beed1b73145b21e8b731df394d9e2be8
Gustavo-C-almeida <105670387+Gustavo-C-almeida@users.noreply.github.com>
2023-05-04 21:58:16 -0300

    Criando barramento de eventos
    Continuar na pagina 45 o serviço de consultas.

--- cifra/index.js
+++ cifra/index.js
@@ -1,13 +1,18 @@
 const express = require ('express');
 const bodyParser = require('body-parser');
-
+const axios = require ('axios');
 const app = express();
 app.use(bodyParser.json());
 
 const cifrasPorMusicaId = {};
 const {v4: uuidv4} = require('uuid');
 
-app.put('/musica/:id/cifra', (req, res) => {
+app.post("/eventos", (req, res) => {
+    console.log(req.body);
+    res.status(200).send({ msg: "ok" });
+});
+
+app.put('/musica/:id/cifra', async (req, res) => {
     const idCifra = uuidv4();
     const { texto } = req.body
     const cifrasDaMusica =
@@ -15,6 +20,12 @@ app.put('/musica/:id/cifra', (req, res) => {
     cifrasDaMusica.push({ id: idCifra, texto });
     cifrasPorMusicaId[req.params.id] =
     cifrasDaMusica;
+    await axios.post('http://localhost:10000/eventos', {
+    tipo: "CifrasCriadas",
+    dados: {
+    id: idCifra, texto, MusicaId: req.params.id
+        }  
+    })
     res.status(201).send(cifrasDaMusica);
 });
 
