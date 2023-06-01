953069047f9dacf484e770db76186c263bc0e518
Gustavo-C-almeida <elias.nery01@Gmail.com>
2023-06-01 18:48:23 -0300

    Adicionando funcionalidade ler mais
    Adicionando funcionalidade ler mais e ler menos para a letra da musica não ocupar o feed.

--- musi-code/src/components/pages/Search.js
+++ musi-code/src/components/pages/Search.js
@@ -5,11 +5,40 @@ import styles from "./Search.module.css";
 import axios from "axios";
 
 function MusicCard({ titulo, autor, letra, fullName }) {
+  const [showFull, setShowFull] = useState(false);
+
+  const handleReadMore = () => {
+    setShowFull(true);
+  };
+
+  const handleShowLess = () => {
+    setShowFull(false);
+  };
+
   return (
     <div className={styles.music_card}>
       <h2>{titulo}</h2>
       <p className={styles.musicAuthor}>Autor: {autor}</p>
-      <p className={styles.musicLyrics}>Letra: {letra}</p>
+      <p className={styles.musicLyrics}>
+        Letra:{" "}
+        {letra.length > 320 && !showFull ? (
+          <>
+            {letra.slice(0, 320)}...{" "}
+            <span className={styles.readMore} onClick={handleReadMore}>
+              Ler mais
+            </span>
+          </>
+        ) : (
+          <>
+            {letra}{" "}
+            {showFull && (
+              <span className={styles.showLess} onClick={handleShowLess}>
+                Mostrar menos
+              </span>
+            )}
+          </>
+        )}
+      </p>
       <p className={styles.musicFullName}>Nome do publicador: {fullName}</p>
     </div>
   );
@@ -22,17 +51,16 @@ function Search() {
     const fetchMusicData = async () => {
       try {
         const response = await axios.post("http://localhost:6001/events", {
-          type: "GetAllMusicas"
+          type: "GetAllMusicas",
         });
         setMusicData(response.data);
       } catch (error) {
         console.error("Erro ao buscar as músicas:", error);
       }
     };
-  
+
     fetchMusicData();
   }, []);
-  
 
   const handleSearch = async (data) => {
     // Atualize o estado do musicData com os dados da busca
@@ -44,7 +72,7 @@ function Search() {
       <div className={styles.display}>
         <FormSearch onSearch={handleSearch} />
       </div>
-      
+
       {musicData.map((music) => (
         <MusicCard
           key={music._id}
